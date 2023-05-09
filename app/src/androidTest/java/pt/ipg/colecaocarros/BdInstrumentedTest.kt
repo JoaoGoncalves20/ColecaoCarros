ee116126fc8767abbba44a67c45487ca308bac5e
João <josantiago2033@gmail.com>
2023-05-09 10:14:17 +0100

    teste para verificar se é posivel abrir a base de dados

--- app/src/androidTest/java/pt/ipg/colecaocarros/BdInstrumentedTest.kt
+++ app/src/androidTest/java/pt/ipg/colecaocarros/BdInstrumentedTest.kt
@@ -14,11 +14,16 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+class BdInstrumentedTest {
+
+    private fun getAppContext() =
+        InstrumentationRegistry.getInstrumentation().targetContext
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("pt.ipg.colecaocarros", appContext.packageName)
+    fun consegueAbrirBaseDados() {
+        val openHelper = BDcolecaocarrosOpenHelper(getAppContext())
+        val bd = openHelper.readableDatabase
+        assert(bd.isOpen)
     }
+
+
 }
