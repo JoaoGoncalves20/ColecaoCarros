5b60c937e4526302d98cf987b2edac6fe0dbcaec
João <josantiago2033@gmail.com>
2023-05-26 09:53:31 +0100

    Inicializar o recycleView dos carros (adapter e layoutManager)

--- app/src/main/java/pt/ipg/colecaocarros/ListaCarrosFragment.kt
+++ app/src/main/java/pt/ipg/colecaocarros/ListaCarrosFragment.kt
@@ -5,28 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import pt.ipg.colecaocarros.databinding.FragmentListaCarrosBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ListaCarrosFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ListaCarrosFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private var _binding: FragmentListaCarrosBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
@@ -37,23 +26,15 @@ class ListaCarrosFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_lista_carros, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val adapterCarros = AdapterCarros()
+        binding.recycleViewCarros.adapter = adapterCarros
+        binding.recycleViewCarros.layoutManager = LinearLayoutManager(requireContext())
+    }
+
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ListaCarrosFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ListaCarrosFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+
     }
 }
