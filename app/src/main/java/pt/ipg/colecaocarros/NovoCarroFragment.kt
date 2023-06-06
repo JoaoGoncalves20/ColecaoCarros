f58400b74ee8f01373e9cf6ed6f0ab42a38f988d
João <josantiago2033@gmail.com>
2023-06-06 10:42:13 +0100

    Adicionar fragmento para criar um novo carro

--- app/src/main/java/pt/ipg/colecaocarros/NovoCarroFragment.kt
+++ app/src/main/java/pt/ipg/colecaocarros/NovoCarroFragment.kt
@@ -6,14 +6,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import pt.ipg.colecaocarros.databinding.FragmentNovoCarroBinding
 import pt.ipg.colecaocarros.databinding.FragmentSobreBinding
 
-/**
- * A simple [Fragment] subclass as the second destination in the navigation.
- */
-class SobreFragment : Fragment() {
 
-    private var _binding: FragmentSobreBinding? = null
+class NovoCarroFragment : Fragment() {
+    private var _binding: FragmentNovoCarroBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -24,17 +22,13 @@ class SobreFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        _binding = FragmentSobreBinding.inflate(inflater, container, false)
+        _binding = FragmentNovoCarroBinding.inflate(inflater, container, false)
         return binding.root
 
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        binding.buttonSecond.setOnClickListener {
-            findNavController().navigate(R.id.action_Sobreragment_to_MenuPrincipalFragment)
-        }
 
         val activity =activity as MainActivity
         activity.fragment = this
